814bb862d72b7c4875e37327314dedeeda6da54d
nHexanol <nHexanol@gmail.com>
2022-11-26 19:02:06 +0000

    Finally!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/main/java/net/Phoenix/api/WynncraftAPI.java
+++ src/main/java/net/Phoenix/api/WynncraftAPI.java
@@ -1,11 +1,13 @@
 package net.Phoenix.api;
 
+import net.Phoenix.Utilities;
 import net.Phoenix.api.objects.Player;
 
 public class WynncraftAPI {
 
-    public void getPlayerStats(String player){
+    public Player getPlayerStats(String player){
         String url = WynncraftEndpoints.PLAYER.getUrl().replace("{PLAYER}", player);
+        return Player.deserialize(Utilities.queryAPI(url));
     }
 
 
