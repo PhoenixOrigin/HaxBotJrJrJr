2e73b9e58921a511b01082650040ab407c4451c7
nHexanol <nHexanol@gmail.com>
2023-02-13 22:18:15 +0000

    Major work in progress
    Took 17 minutes

--- src/main/java/net/Phoenix/api/PlayerRequest.java
+++ src/main/java/net/Phoenix/api/PlayerRequest.java
@@ -1,4 +1,34 @@
 package net.Phoenix.api;
 
-public class PlayerRequest {
+import com.google.common.util.concurrent.RateLimiter;
+import net.Phoenix.api.objects.Player;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+public class PlayerRequest implements Callable<Player> {
+
+    private String player;
+    private RateLimiter second;
+    private RateLimiter minute;
+
+    public PlayerRequest(String player, RateLimiter second, RateLimiter minute){
+        this.player = player;
+        this.second = second;
+        this.minute = minute;
+    }
+
+    @Override
+    public Player call() throws Exception{
+        second.acquire();
+        minute.acquire();
+
+        WynncraftAPI api = new WynncraftAPI();
+        try {
+            return api.getPlayerStats(player);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
--- src/main/java/net/Phoenix/api/WynncraftAPI.java
+++ src/main/java/net/Phoenix/api/WynncraftAPI.java
@@ -5,6 +5,12 @@ import net.Phoenix.utilities.Utilities;
 import net.Phoenix.api.objects.Player;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class WynncraftAPI {
 
@@ -13,13 +19,29 @@ public class WynncraftAPI {
         return Player.deserialize(Utilities.queryAPI(url));
     }
 
-    public void getPlayersStats(String... players) throws IOException {
-        for(String player : players){
-            String url = WynncraftEndpoints.PLAYER.getUrl().replace("{PLAYER}", player);
+    public static List<Player> getPlayer(List<String> playerNames) throws InterruptedException {
+        RateLimiter second = RateLimiter.create(50);
+        RateLimiter minute = RateLimiter.create(180, 0, TimeUnit.MINUTES);
 
+        int numberOfRequests = playerNames.size();
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfRequests);
+        List<Future<Player>> futures = new ArrayList<>();
+        for (String playerName : playerNames) {
+            PlayerRequest requestThread = new PlayerRequest(playerName, second, minute);
+            Future<Player> future = executor.submit(requestThread);
+            futures.add(future);
         }
+        List<Player> results = new ArrayList<>();
+        for (Future<Player> future : futures) {
+            try {
+                results.add(future.get());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        executor.shutdown();
+        return results;
     }
-
 
     public enum WynncraftEndpoints {
         PLAYER("https://api.wynncraft.com/v2/player/{PLAYER}/stats");
