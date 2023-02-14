774f6544560a100ded622f85ac4d07dcdafa0616
nHexanol <nHexanol@gmail.com>
2023-02-14 17:09:31 +0000

    Optimsation of rrl class
    Took 36 minutes

--- src/main/java/net/Phoenix/utilities/ResourceRateLimit.java
+++ src/main/java/net/Phoenix/utilities/ResourceRateLimit.java
@@ -1,43 +1,85 @@
 package net.Phoenix.utilities;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ResourceRateLimit {
 
-    private final int numDosesPerSecond;
-    private final int numDosesPerMinute;
+    private final int numDosesPerSecond = 50;
+    private final int numDosesPerMinute = 180;
+    private final AtomicInteger secondDosesReleased = new AtomicInteger(0);
+    private final AtomicInteger minuteDosesReleased = new AtomicInteger(0);
+    private final AtomicInteger remainingDosesSecond = new AtomicInteger(numDosesPerSecond);
+    private final AtomicInteger remainingDosesMinute = new AtomicInteger(numDosesPerMinute);
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
+    private final ScheduledFuture<?> minutes;
+    private final ScheduledFuture<?> seconds;
+    private long secondsUpdateMillis = 0;
+    private long minutesUpdateMillis = 0;
+    private final Map<Long, Long> consumedIds = new HashMap<>();
 
-    private final AtomicInteger remainingDosesSecond;
-    private final AtomicInteger remainingDosesMinute;
-
-
-
-    public ResourceRateLimit(int numDosesPerSecond, int seconds, int numDosesPerMinute, int minutes) {
-        this.numDosesPerSecond = numDosesPerSecond;
-        this.remainingDosesSecond = new AtomicInteger(this.numDosesPerSecond);
-        this.numDosesPerMinute = numDosesPerMinute;
-        this.remainingDosesMinute = new AtomicInteger(this.numDosesPerMinute);
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
-        scheduler.scheduleAtFixedRate(this::refillSeconds, 0, seconds, TimeUnit.SECONDS);
-        scheduler.scheduleAtFixedRate(this::refillSeconds, 0, minutes, TimeUnit.MINUTES);
+    public ResourceRateLimit() {
+        seconds = scheduler.scheduleAtFixedRate(this::refillSeconds, 0, 1, TimeUnit.SECONDS);
+        minutes = scheduler.scheduleAtFixedRate(this::refillMinutes, 0, 1, TimeUnit.MINUTES);
     }
 
-    public void refillSeconds(){
-        remainingDosesSecond.set(numDosesPerSecond);
+    private void refillSeconds() {
+        int released = secondDosesReleased.getAndSet(0);
+        remainingDosesSecond.addAndGet(secondDosesReleased.get());
+        secondsUpdateMillis = System.nanoTime();
     }
 
-    public void refillMinutes(){
-        remainingDosesMinute.set(numDosesPerMinute);
+    private void refillMinutes() {
+        int released = minuteDosesReleased.getAndSet(0);
+        remainingDosesMinute.addAndGet(minuteDosesReleased.get());
+        minutesUpdateMillis = System.nanoTime();
     }
 
-    public void consume() {
-        while (remainingDosesSecond.get() == 0 || remainingDosesMinute.get() == 0) {
+    public long consume() throws InterruptedException {
+        while (true) {
+            int remainingSeconds = remainingDosesSecond.get();
+            int remainingMinutes = remainingDosesMinute.get();
+
+            if (remainingSeconds == 0 || remainingMinutes == 0) {
+                long minutesDelay = minutes.getDelay(TimeUnit.SECONDS);
+                long secondsDelay = seconds.getDelay(TimeUnit.SECONDS);
+
+                if (minutesDelay < 0 && secondsDelay < 0) {
+                    Thread.onSpinWait();
+                } else if (minutesDelay > secondsDelay) {
+                    Thread.sleep(Duration.ofSeconds(minutesDelay).toMillis());
+                } else {
+                    Thread.sleep(Duration.ofSeconds(secondsDelay).toMillis());
+                }
+            } else {
+                remainingDosesSecond.decrementAndGet();
+                remainingDosesMinute.decrementAndGet();
+
+                long id = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+                consumedIds.put(id, System.currentTimeMillis());
+                return id;
+            }
         }
-        remainingDosesSecond.decrementAndGet();
-        remainingDosesMinute.decrementAndGet();
+    }
+
+    public void release(long id) {
+        Long consumed = consumedIds.remove(id);
+        if (consumed == null) {
+            return;
+        }
+        if (!(consumed < secondsUpdateMillis)) {
+            secondDosesReleased.incrementAndGet();
+        } else if (!(consumed < minutesUpdateMillis)) {
+            minuteDosesReleased.incrementAndGet();
+        }
+    }
+
+    public void shutdown() {
+        scheduler.shutdown();
     }
 
 }
